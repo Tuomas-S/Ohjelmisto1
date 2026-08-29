9e46654b6e5a340ab52aada240f7f83803d6e32b
Tuomas Soini <tuomsoi@metropolia.fi>
2026-08-29 20:53:54 +0300

    pieni virhekorjaus tehtava_4.4.py

--- mod04/tehtävä_4.4.py
+++ mod04/tehtävä_4.4.py
@@ -1,9 +1,9 @@
 vuosi = int(input("Anna vuosiluku.\n"))
 
 if vuosi % 4 == 0:
-    if vuosi % 400 == 0:
-        print("\nVuosi on karkausvuosi.")
-    else:
+    if vuosi % 100 == 0 and not vuosi % 400 == 0:
         print("\nVuosi ei ole karkausvuosi.")
+    else:
+        print("\nVuosi on karkausvuosi.")
 else:
     print("\nVuosi ei ole karkausvuosi.")
