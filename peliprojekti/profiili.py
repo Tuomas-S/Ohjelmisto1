572b07508268363e3fdfddb68fdd0ff13b89f48d
Tuomas Soini <tuomsoi@metropolia.fi>
2026-08-29 20:35:49 +0300

    Pieni muutos profiiliohjelmaan

--- peliprojekti/profiili.py
+++ peliprojekti/profiili.py
@@ -6,12 +6,8 @@ while True:
     try:
         int(ikä)
     except ValueError:
-        ikä = input("\nAnna oikea ikä.\n")
+        ikä = input("\nAnna oikea ikä:\n")
     else:
         break
 
 ikä = int(ikä)
-if ikä < 12:
-    print("\nTämä peli on K12.")
-else:
-    print("\nTervetuloa!")
